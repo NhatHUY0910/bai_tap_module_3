a50c38667c91a5d1cb70242d4994ad53ef4c0fe0
NhatHUY0910 <157088439+NhatHUY0910@users.noreply.github.com>
2024-04-25 10:49:05 +0700

    Update DiscountServlet.java

--- baitap_trenlop/demo1/src/main/java/org/example/demo1/DiscountServlet.java
+++ baitap_trenlop/demo1/src/main/java/org/example/demo1/DiscountServlet.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 
 @WebServlet("/discount")
 public class DiscountServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
