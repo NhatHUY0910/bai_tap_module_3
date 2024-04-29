4655ac7989e333faa870d761f918c7f7cd16a0ce
NhatHUY0910 <157088439+NhatHUY0910@users.noreply.github.com>
2024-04-29 21:09:22 +0700

    Delete baitap_trenlop/case1 directory

--- baitap_trenlop/case1/src/main/java/com/case1/model/entity/IProductService.java
+++
@@ -1,8 +0,0 @@
-package com.case1.model.entity;
-
-public interface IProductService {
-    void addProduct(Product product);
-    void showAllProducts();
-    void updateProduct(Product product);
-    void deleteProduct(Product product);
-}
